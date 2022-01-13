4149647f181c6183fd236bd3a34579ba33b8a938
Seunghun Lee <hun2456@hanmail.net>
2022-01-13 16:31:21 +0900

    #fix: 로그인 url 변

--- src/main/java/kh/spring/controller/MemberController.java
+++ src/main/java/kh/spring/controller/MemberController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/member/")
 public class MemberController {
 
 	@RequestMapping("login")

b5b72c210edce0f1e70064b2124d154cf332b676
JO <craving4m@gmail.com>
2022-01-13 15:55:18 +0900

    feat: 공지사항 게시판 리스트 기능 구현(selectAll)

---
+++ src/main/java/kh/spring/controller/NoticeController.java
@@ -0,0 +1,35 @@
+package kh.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kh.spring.dao.NoticeDAO;
+import kh.spring.dto.NoticeDTO;
+
+@RequestMapping("/notice/")
+@Controller
+public class NoticeController {
+	
+	@Autowired
+    public NoticeDAO dao;
+	
+	@RequestMapping("toNotice")
+	public String notice(Model model) throws Exception {
+		List<NoticeDTO> notices = dao.selectAll();
+		System.out.println(notices.get(0).getMember_id());
+		model.addAttribute("notices", notices);
+	    return "/notice/noticeList";
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(Exception e) {
+		e.printStackTrace();
+		return "redirect:/";
+	}
+
+}

eaf987584eb61dbb485ff4d95afe327adf48fb6f
JO <craving4m@gmail.com>
2022-01-14 14:56:56 +0900

    fix: NoticeContoller, NoticeDAO, NoticeService에서 field 주입에서 생성자 주입으로 변경

--- src/main/java/kh/spring/dao/NoticeDAO.java
+++ src/main/java/kh/spring/dao/NoticeDAO.java
@@ -3,16 +3,18 @@ package kh.spring.dao;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kh.spring.dto.NoticeDTO;
 
 @Repository
 public class NoticeDAO {
-
-	@Autowired
-	private SqlSessionTemplate mybatis;
+	
+	private final SqlSessionTemplate mybatis;
+	
+	public NoticeDAO(SqlSessionTemplate mybatis) {
+		this.mybatis = mybatis;
+	}
 	
 	public List<NoticeDTO> selectAll() {
 		return mybatis.selectList("Notice.selectAll");

c164c6a1aebadb3a3b98f4a10f1692f580d47729
jhleeeeeeee <gov987@naver.com>
2022-01-16 17:46:29 +0900

    fix: 컨플릭트 해결

--- src/main/java/kh/spring/dao/MemberDAO.java
+++ src/main/java/kh/spring/dao/MemberDAO.java
@@ -72,22 +72,6 @@ public class MemberDAO {
 		
 		return mybatis.update("Member.resetPassword", map);
 		}
-	
-	public int insertMember(MemberDTO dto) {
-		
-		Map<String,String> map = new HashMap<>();
-		map.put("member_username", dto.getMember_username());
-		map.put("member_password", bCrptPasswordEncoder.encode((dto.getMember_password())));
-		map.put("member_name", dto.getMember_name());
-		map.put("member_birth_date", dto.getMember_birth_date());
-		map.put("member_phone", dto.getMember_phone());
-		map.put("member_zipcode", dto.getMember_zipcode());
-		map.put("member_address1", dto.getMember_address1());
-		map.put("member_address2", dto.getMember_address2());
-		map.put("member_email", dto.getMember_email());
-				
-		return mybatis.insert("Member.insertMember",map);
-	}
-	
+
 	}
 

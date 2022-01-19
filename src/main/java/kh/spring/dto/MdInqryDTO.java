7fc7785742333506ee1ad594beab272ebbf37ea3
Juyoung Park <86351381+kkongkeozzang@users.noreply.github.com>
2022-01-20 05:03:53 +0900

    feat: 상품 후기 게시판 조회수 증가 구현 (#32)
    * fix: 상품 리스트 정렬 페이징 숨어들어가는 거 수정
    
    * refact: PageNavigator DAO 변수 삭제
    
    fix: 상품 최신 정렬 페이징 클릭 시 데이터 안받아오는 문제 수정
    
    * refact: 상품 페이징 중복 코드 제거
    
    mdList.jsp 자바스크립트 코드 중복 제거
    MdController 중복 코드 제거
    PageNavigator 중복 코드 제거
    MdService 및 MdDAO 중복 코드 제거
    
    * feat: 상품 지역별 분류, 정렬 기능 구현
    
    * feat: 상품 상세페이지 UI 구현
    
    MdDetailAPIController 생성
    MdDetailController 생성
    mdDetail.jsp 생성
    상품 정보 보여주기
    수량, 장바구니, 찜, 즉시구매 버튼 구현(기능 미구현)
    상세 페이지 상품과 같은 지역의 상품들을 캐러셀로 보여줌
    
    * feat: 상품 상세페이지 탭 안에 게시판 UI 구현
    
    * feat: 상품 후기 게시판 페이징 구현
    
    * feat: 상품 후기 정렬 기능 구현
    
    * fix: 상품 후기 게시판 내용 보이기 기능 수정
    
    * style: jsp랑 DAO 코드 살짝 수정
    
    * refact: PageNavigator 클래스 필요없는 메소드 삭제
    
    * chore: mdDetail.jsp 컨플릭트 해결
    
    * chore: mdDetail.jsp 컨플릭트 수정
    
    * chore: mdDetail.jsp 컨플릭트 수정
    
    * feat: 상품 문의 페이지 구현
    
    상품 문의 제목 목록 보여주기
    다른 제목 클릭하면 기존 열려있는 창은 닫힘
    상품 문의에 답변이 있는 경우 내용에 답변 추가
    없는 경우 보여주지 않는다
    페이징 구현
    
    * feat: CustonAuthenticationSuccessHandler 생성
    
    * feat: 상품 리뷰 조회수 실시간 카운트 구현(중복방지)

---
+++ src/main/java/kh/spring/dto/MdInqryDTO.java
@@ -0,0 +1,116 @@
+package kh.spring.dto;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class MdInqryDTO {
+	
+	private int sort_md_question_id;
+	private int md_id;
+	private String md_question_username;
+	private String md_question_title;
+	private String md_question_content;
+	private Timestamp md_question_write_date;
+	private String md_question_reply_yn;
+	
+	private String md_response_username;
+	private String md_response_content;
+	private Timestamp md_response_write_date;
+	
+	public MdInqryDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MdInqryDTO(int sort_md_question_id, int md_id, String md_question_username, String md_question_title,
+			String md_question_content, Timestamp md_question_write_date, String md_question_reply_yn,
+			String md_response_username, String md_response_content, Timestamp md_response_write_date) {
+		super();
+		this.sort_md_question_id = sort_md_question_id;
+		this.md_id = md_id;
+		this.md_question_username = md_question_username;
+		this.md_question_title = md_question_title;
+		this.md_question_content = md_question_content;
+		this.md_question_write_date = md_question_write_date;
+		this.md_question_reply_yn = md_question_reply_yn;
+		this.md_response_username = md_response_username;
+		this.md_response_content = md_response_content;
+		this.md_response_write_date = md_response_write_date;
+	}
+	public int getSort_md_question_id() {
+		return sort_md_question_id;
+	}
+	public void setSort_md_question_id(int sort_md_question_id) {
+		this.sort_md_question_id = sort_md_question_id;
+	}
+	public int getMd_id() {
+		return md_id;
+	}
+	public void setMd_id(int md_id) {
+		this.md_id = md_id;
+	}
+	public String getMd_question_username() {
+		return md_question_username;
+	}
+	public void setMd_question_username(String md_question_username) {
+		this.md_question_username = md_question_username;
+	}
+	public String getMd_question_title() {
+		return md_question_title;
+	}
+	public void setMd_question_title(String md_question_title) {
+		this.md_question_title = md_question_title;
+	}
+	public String getMd_question_content() {
+		return md_question_content;
+	}
+	public void setMd_question_content(String md_question_content) {
+		this.md_question_content = md_question_content;
+	}
+	public Timestamp getMd_question_write_date() {
+		return md_question_write_date;
+	}
+	public void setMd_question_write_date(Timestamp md_question_write_date) {
+		this.md_question_write_date = md_question_write_date;
+	}
+	public String getMd_question_reply_yn() {
+		return md_question_reply_yn;
+	}
+	public void setMd_question_reply_yn(String md_question_reply_yn) {
+		this.md_question_reply_yn = md_question_reply_yn;
+	}
+	public String getMd_response_username() {
+		return md_response_username;
+	}
+	public void setMd_response_username(String md_response_username) {
+		this.md_response_username = md_response_username;
+	}
+	public String getMd_response_content() {
+		return md_response_content;
+	}
+	public void setMd_response_content(String md_response_content) {
+		this.md_response_content = md_response_content;
+	}
+	public Timestamp getMd_response_write_date() {
+		return md_response_write_date;
+	}
+	public void setMd_response_write_date(Timestamp md_response_write_date) {
+		this.md_response_write_date = md_response_write_date;
+	}
+	
+	public String getQuestionFormedDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+		if(getMd_question_write_date() == null) {
+			return null;
+		}
+		return sdf.format(getMd_question_write_date().getTime());
+	}
+	public String getResponseFormedDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+		if(getMd_response_write_date() == null) {
+			return null;
+		}
+		return sdf.format(getMd_response_write_date().getTime());
+	}
+	
+}
+

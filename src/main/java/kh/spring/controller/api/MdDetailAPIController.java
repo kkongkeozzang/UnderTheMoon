aa6dc5d9456c61124908dc2ea7e0f88a95a8304b
Juyoung Park <86351381+kkongkeozzang@users.noreply.github.com>
2022-01-21 21:51:51 +0900

    feat: 상품 리스트 페이지 및 디테일 페이지 완성 (#44)
    * fix: 상품 리스트 정렬 페이징 숨어들어가는 거 수정
    
    * refact: PageNavigator DAO 변수 삭제
    
    fix: 상품 최신 정렬 페이징 클릭 시 데이터 안받아오는 문제 수정
    
    * refact: 상품 페이징 중복 코드 제거
    
    mdList.jsp 자바스크립트 코드 중복 제거
    MdController 중복 코드 제거
    PageNavigator 중복 코드 제거
    MdService 및 MdDAO 중복 코드 제거
    
    * feat: 상품 지역별 분류, 정렬 기능 구현
    
    * feat: 상품 상세페이지 UI 구현
    
    MdDetailAPIController 생성
    MdDetailController 생성
    mdDetail.jsp 생성
    상품 정보 보여주기
    수량, 장바구니, 찜, 즉시구매 버튼 구현(기능 미구현)
    상세 페이지 상품과 같은 지역의 상품들을 캐러셀로 보여줌
    
    * feat: 상품 상세페이지 탭 안에 게시판 UI 구현
    
    * feat: 상품 후기 게시판 페이징 구현
    
    * feat: 상품 후기 정렬 기능 구현
    
    * fix: 상품 후기 게시판 내용 보이기 기능 수정
    
    * style: jsp랑 DAO 코드 살짝 수정
    
    * refact: PageNavigator 클래스 필요없는 메소드 삭제
    
    * chore: mdDetail.jsp 컨플릭트 해결
    
    * chore: mdDetail.jsp 컨플릭트 수정
    
    * chore: mdDetail.jsp 컨플릭트 수정
    
    * feat: 상품 문의 페이지 구현
    
    상품 문의 제목 목록 보여주기
    다른 제목 클릭하면 기존 열려있는 창은 닫힘
    상품 문의에 답변이 있는 경우 내용에 답변 추가
    없는 경우 보여주지 않는다
    페이징 구현
    
    * feat: CustonAuthenticationSuccessHandler 생성
    
    * feat: 상품 리뷰 조회수 실시간 카운트 구현(중복방지)
    
    * fix: 상세페이지 양조절 버튼 수정
    
    * fix: 장바구니 상품 중복 제거
    
    * feat: 최근 본 상품 UI 구현
    
    * refact: autowired 어노테이션 삭제 및 자잘한 수정
    
    * feat: 쿠키에 최근 본 상품번호 저장 로직 구현
    
    * pull 전 commit
    
    * chore: DB 코드 따로 뺌
    
    * feat: 최근 본 상품, 상품 디테일 페이지 관련 상품 누를때 화면 전환 구현
    
    * refactor: 최근 본 상품 쿠키 저장 갯수 제한

--- src/main/java/kh/spring/controller/api/MdDetailAPIController.java
+++ src/main/java/kh/spring/controller/api/MdDetailAPIController.java
@@ -1,7 +1,5 @@
 package kh.spring.controller.api;
 
-import java.util.HashMap;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/md/detail/rest/")
 public class MdDetailAPIController {
-	
-
-	@RequestMapping(value = "detailPage", produces = "application/json")
-	public HashMap<String, Object> detailPage(String md_id) {
-		HashMap<String, Object> result = new HashMap<>();
-		return result;
-	}
 	
 }

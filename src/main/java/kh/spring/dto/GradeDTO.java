07114d9a5148cc3a8b7aa9f682054f703de38050
craving4u <95415147+craving4u@users.noreply.github.com>
2022-01-24 14:41:04 +0900

    #feat: 등급관리 기능 구현 (#52)
    * @fix: pageNavigator 수정
    - navigator 없을 때 뜨는 메시지 sysout으로 수정
    - home.jsp에 판매자 페이지 링크 추가
    - SellerController 추가
    
    * #feat: 등급관리 CRUD 기능 구현
    
    * #fix : root-context.xml 머지함
    
    * #fix: 등급관리 기능 중 수정 기능 오류 수정 및 입력, 수정 Regex추가
    
    * #fix: input type text 입력 자릿수 제한
    
    * #fix: 등급 관리 수정
    - 컬럼 변경으로 인한 입력, 조회, 수정 정보 추가
    - 아이디 입력시 사용가능 여부 체크기능 추가
    - 페이징 else 제거

--- src/main/java/kh/spring/dto/GradeDTO.java
+++ src/main/java/kh/spring/dto/GradeDTO.java
@@ -1,54 +1,55 @@
 package kh.spring.dto;
 
 public class GradeDTO {
-	
-	private String grade_name;
-	private double grade_percent;
-	private int grade_target;
-	private String grade_coupon;
-	
-	public GradeDTO() {
-		super();
-	}
+   
+   private String grade_name;
+   private double grade_percent;
+   private int grade_target;
+   private String grade_coupon;
+   
+   public GradeDTO() {
+      super();
+   }
 
-	public GradeDTO(String grade_name, double grade_percent, int grade_target, String grade_coupon) {
-		super();
-		this.grade_name = grade_name;
-		this.grade_percent = grade_percent;
-		this.grade_target = grade_target;
-		this.grade_coupon = grade_coupon;
-	}
+   public GradeDTO(String grade_name, double grade_percent, int grade_target, String grade_coupon) {
+      super();
+      this.grade_name = grade_name;
+      this.grade_percent = grade_percent;
+      this.grade_target = grade_target;
+      this.grade_coupon = grade_coupon;
+   }
 
-	public String getGrade_name() {
-		return grade_name;
-	}
+   public String getGrade_name() {
+      return grade_name;
+   }
 
-	public void setGrade_name(String grade_name) {
-		this.grade_name = grade_name;
-	}
+   public void setGrade_name(String grade_name) {
+      this.grade_name = grade_name;
+   }
 
-	public double getGrade_percent() {
-		return grade_percent;
-	}
+   public double getGrade_percent() {
+      return grade_percent;
+   }
 
-	public void setGrade_percent(double grade_percent) {
-		this.grade_percent = grade_percent;
-	}
+   public void setGrade_percent(double grade_percent) {
+      this.grade_percent = grade_percent;
+   }
 
-	public int getGrade_target() {
-		return grade_target;
-	}
+   public int getGrade_target() {
+      return grade_target;
+   }
 
-	public void setGrade_target(int grade_target) {
-		this.grade_target = grade_target;
-	}
+   public void setGrade_target(int grade_target) {
+      this.grade_target = grade_target;
+   }
 
-	public String getGrade_coupon() {
-		return grade_coupon;
-	}
+   public String getGrade_coupon() {
+      return grade_coupon;
+   }
 
-	public void setGrade_coupon(String grade_coupon) {
-		this.grade_coupon = grade_coupon;
-	}
-		
+   public void setGrade_coupon(String grade_coupon) {
+      this.grade_coupon = grade_coupon;
+   }
+      
 }
+
---
+++ src/main/java/kh/spring/service/SellerService.java
@@ -0,0 +1,38 @@
+package kh.spring.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kh.spring.dao.SellerDAO;
+import kh.spring.dto.GradeDTO;
+
+@Service
+public class SellerService {
+
+	private final SellerDAO sellerDAO;
+	
+	public SellerService(SellerDAO sellerDAO) {
+		this.sellerDAO = sellerDAO;
+	}
+	
+	public List<GradeDTO> selectAllGrade(){
+		return sellerDAO.selectAllGrade();
+	}
+	
+	public int insertGrade(GradeDTO grades){
+		return sellerDAO.insertGrade(grades);
+	}
+	
+	public int deleteGrade(String grade_name){
+		return sellerDAO.deleteGrade(grade_name);
+	}
+	
+	public int updateGrade(GradeDTO grades){
+		return sellerDAO.updateGrade(grades);
+	}
+	
+	public int checkGrade(String grade_name){
+		return sellerDAO.checkGrade(grade_name);
+	}
+}

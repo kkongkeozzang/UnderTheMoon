bd9017afb72a55304c8f6d4077aaab57eadbb0fb
KKONGKEOZZANG <suzie1642@gmail.com>
2022-01-26 20:34:22 +0900

    style: 주석 코드 삭제

--- src/main/java/kh/spring/controller/api/AdminAPIController.java
+++ src/main/java/kh/spring/controller/api/AdminAPIController.java
@@ -25,49 +25,5 @@ public class AdminAPIController {
 		return new ResponseEntity<Integer>(result,HttpStatus.OK);
 	}
 	
-	
-	
-//	@PostMapping("addToCart")
-//	public ResponseEntity<Integer> addToCart(String member_username,Integer md_id, Integer cart_item_count){
-//		
-//		Integer result = cartService.addToCart(member_username,md_id,cart_item_count);
-//		
-//		return new ResponseEntity<Integer>(result,HttpStatus.OK);
-//		
-//	}
-//	
-//	@DeleteMapping("deleteCart/{cart_id}")
-//	public ResponseEntity<Integer> deleteCart(@PathVariable String cart_id){
-//		
-//		Integer result = cartService.deleteCart(Integer.valueOf(cart_id));
-//	
-//		return new ResponseEntity<Integer>(result,HttpStatus.OK);
-//	}
-//	
-//	@PatchMapping("addItemCount/{cart_id}")
-//	public ResponseEntity<Integer> addItemCount(@PathVariable String cart_id){
-//		
-//		Integer result = cartService.addItemCount(Integer.valueOf(cart_id));
-//		
-//		return new ResponseEntity<Integer>(result,HttpStatus.OK);
-//	}
-//	
-//	@PatchMapping("subtractItemCount/{cart_id}")
-//	public ResponseEntity<Integer> subtractItemCount(@PathVariable String cart_id){
-//		
-//		Integer result = cartService.subtractItemCount(Integer.valueOf(cart_id));
-//		
-//		return new ResponseEntity<Integer>(result,HttpStatus.OK);
-//	}
-//	
-//	@DeleteMapping("deleteAll/{member_id}")
-//	public ResponseEntity<Integer> deleteAll(@PathVariable String member_id){
-//
-//		Integer result = cartService.deleteAll(Integer.valueOf(member_id));
-//	
-//		return new ResponseEntity<Integer>(result,HttpStatus.OK);
-//	}
-	
-	
 
 }

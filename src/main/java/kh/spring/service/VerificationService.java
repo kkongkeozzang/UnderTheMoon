1a93bfb940606b10293ceb1a1fdfbec1529a3e3c
SlimskiTheWise <87960584+SlimskiTheWise@users.noreply.github.com>
2022-02-03 19:58:19 +0900

    "fix: 비밀번호 찾기 수정 (#100)
    * fix: 비밀번호 찾기 수정

--- src/main/java/kh/spring/service/VerificationService.java
+++ src/main/java/kh/spring/service/VerificationService.java
@@ -19,7 +19,7 @@ public class VerificationService {
         // 4 params(to, from, type, text) are mandatory. must be filled
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("to", member_phone);    // 수신전화번호
-        params.put("from", "전화번호.");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("from", "전화번호..");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "SMS");
         params.put("text", " 휴대폰인증 테스트 메시지 : 인증번호는" + "["+numStr+"]" + "입니다.");
         params.put("app_version", "test app 1.2"); // application name and version

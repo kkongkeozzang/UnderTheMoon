73af557eb0fac9a1d1aa1a69d15b4681ebf13df6
Juyoung Park <86351381+kkongkeozzang@users.noreply.github.com>
2022-02-06 17:00:17 +0900

    fix: 상품 검색 페이지 잘못 수정한거 수정 (#143)
    * fix: 상품 검색 페이지 최근본상품 수정
    
    * chore: pr안되서 커밋

--- src/main/java/kh/spring/controller/MdReviewController.java
+++ src/main/java/kh/spring/controller/MdReviewController.java
@@ -1,14 +1,8 @@
 package kh.spring.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kh.spring.dto.MdReviewDTO;
-import kh.spring.dto.MemberDTO;
-import kh.spring.service.MdReviewService;
-import kh.spring.service.MemberService;
 
 @Controller
 @RequestMapping("/md/detail/review/")
